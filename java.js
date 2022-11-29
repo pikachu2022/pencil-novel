b1254bdc8675182d165a2da4dc37e64ebc505741
pikachu2022 <harrison.wu2003@gmail.com>
2022-11-29 20:46:40 +0800

    Update java.js

--- java.js
+++ java.js
@@ -13,4 +13,21 @@ function op(){
   var x=document.getElementById("pen").textContent;
   //window.alert(x);
   window.open(x);
+var a=Math.floor(Math.random() * 2)+1;
+var b=Math.floor(Math.random() * 10);
+var c=Math.floor(Math.random() * 10);
+var d=Math.floor(Math.random() * 10);
+var e=Math.floor(Math.random() * 10);
+var f=Math.floor(Math.random() * 10);
+  if(a>1)b=Math.floor(Math.random() * 4);
+var res="https://www.23qb.net/book/";
+ document.getElementById("pen").textContent=res+a+b+c+d+e+f+"/";
 }
+var a=Math.floor(Math.random() * 1)+1;
+var b=Math.floor(Math.random() * 10);
+var c=Math.floor(Math.random() * 10);
+var d=Math.floor(Math.random() * 10);
+var e=Math.floor(Math.random() * 10);
+var f=Math.floor(Math.random() * 10);
+var res="https://www.23qb.net/book/";
+document.getElementById("pen").textContent=res+a+b+c+d+e+f+"/";
